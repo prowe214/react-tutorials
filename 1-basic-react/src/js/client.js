aafeefa2d2eb60302b64f25c83e412fd8c4b5814
Paul Rowe <paulcullenrowe@The-Battle-Machine.local>
2016-06-23 14:02:33 -0600

    setup complete

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,1 +1,14 @@
-console.log("hi");
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    render () {
+        return (
+            <h1>It Works!</h1>
+        )
+    }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);

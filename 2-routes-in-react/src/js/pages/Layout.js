f6e92688084a0fe568dff2a0894bb73e27592a35
Paul Rowe <paulcullenrowe@The-Battle-Machine.local>
2016-06-24 16:20:45 -0600

    basic routing

--- 2-routes-in-react/src/js/pages/Layout.js
+++ 2-routes-in-react/src/js/pages/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Layout extends React.Component {
+    navigate() {
+        this.props.history.replaceState(null, '/');
+    }
     render () {
         return (
             <div>
                 <h1>REACT</h1>
+                {this.props.children}
+                <Link to="archives" class="btn btn-danger">archives</Link>
+                <Link to="settings"><button class="btn btn-success">settings</button></Link>
+                <button onClick={this.navigate.bind(this)} >featured</button>
             </div>
         )
     }
